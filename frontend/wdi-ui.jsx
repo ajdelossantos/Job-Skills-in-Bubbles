b01e387611e05058541175c538fcda5b656766a6
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-12-04 06:04:26 -0800

    Create frontend skeleton, webpack.config.js, and entry point

---
+++ frontend/wdi-ui.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(
+    <h2>Welcome to the Workforce Data Initiative's Open Skills API!</h2>,
+    root
+  );
+});
