8e00fa18f072d78fbb6bfcdbab03019fea487553
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-12-05 09:52:01 -0800

    Fetch uuid implemented

--- frontend/scripts/job_search.js
+++ frontend/scripts/job_search.js
@@ -5,7 +5,27 @@ const fetchNormalizedJobs = jobString =>
     dataType: "json"
   });
 
-$("#search").keyup(function() {
+const fetchJobSkills = uuid =>
+  $.ajax({
+    method: "GET",
+    url: `http://api.dataatwork.org/v1/jobs/${uuid}/related_skills`,
+    dataType: "json"
+  });
+
+const displayJobs = payload => {
+  $.each(payload, function() {
+    $.each(this, function(k, v) {
+      if (k === "uuid") {
+        let jobUuid = v;
+        return console.log(jobUuid);
+      }
+    });
+  });
+};
+
+$("#search-btn").click(function() {
   var searchField = $("#search").val();
-  fetchNormalizedJobs(searchField).then(response => console.log(response));
+  fetchNormalizedJobs(searchField).then(response => displayJobs(response));
 });
+
+$("#search-results ul").append();
