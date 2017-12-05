d1691a5f8716adf71801b9889a7ad02e7f2edc42
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2017-12-05 10:37:31 -0800

    Fetch skills implemented

--- frontend/scripts/job_search.js
+++ frontend/scripts/job_search.js
@@ -5,6 +5,12 @@ const fetchNormalizedJobs = jobString =>
     dataType: "json"
   });
 
+const searchJob = jobString =>
+  $.ajax({
+    method: "GET",
+    url: `http://api.dataatwork.org/v1/jobs/autocomplete?contains=${jobString}`
+  });
+
 const fetchJobSkills = uuid =>
   $.ajax({
     method: "GET",
@@ -12,18 +18,29 @@ const fetchJobSkills = uuid =>
     dataType: "json"
   });
 
+let currentJobUuid = [];
+
 const displayJobs = payload => {
-  $.each(payload, function() {
+  $.each(payload, function(index) {
     $.each(this, function(k, v) {
       if (k === "uuid") {
-        let jobUuid = v;
-        return console.log(jobUuid);
+        currentJobUuid.push(v);
+        console.log(currentJobUuid);
+        return receiveJobSkills();
       }
     });
   });
 };
 
+const receiveJobSkills = () => {
+  if (currentJobUuid.length > 0) {
+    let uuid = currentJobUuid[0];
+    fetchJobSkills(uuid).then(response => console.log(response));
+  }
+};
+
 $("#search-btn").click(function() {
+  currentJobUuid = [];
   var searchField = $("#search").val();
   fetchNormalizedJobs(searchField).then(response => displayJobs(response));
 });
