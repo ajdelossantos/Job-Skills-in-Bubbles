b10ed4b6e09500553ba317b1bdb44fa103fbf138
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-01-16 13:26:08 -0800

    Revert pinger

--- frontend/wdi-ui.js
+++ frontend/wdi-ui.js
@@ -2,11 +2,6 @@ import * as JobSearch from "./scripts/job_search";
 import * as d3 from "d3";
 
 document.addEventListener("DOMContentLoaded", () => {
-  var http = require("http");
-  setInterval(function() {
-    http.get("https://ajdelossantos.github.io/Job-Skills-in-Bubbles/");
-  }, 300000); // every 5 minutes (300000)
-
   $("#search-btn").click(function() {
     var searchField = $("#search").val();
     JobSearch.fetchNormalizedJob(searchField).then(response =>
